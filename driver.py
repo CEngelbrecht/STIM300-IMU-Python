a99c04c09f160599ab0039886a5b7aaa0016ea85
karver8 <jacobmcarver@gmail.com>
2017-03-15 13:08:08 -0700

    Reads IMU data(0x93)
    Working STIM300 receiver, for windows

--- driver.py
+++ driver.py
@@ -5,10 +5,8 @@ import time
 
 class StimInterface(object):
     _baudrate = 921600
-    _read_length = 37   #40 - 3 (2 for lf and cr, and one for header)
-    _gyro = None
-    _acc = None
-    _inc = None
+    _read_length = 39   #40 - 3 (2 for lf and cr, and one for header)
+    null_byte = b'\x00'
 
     def __init__(self):
         #self.serial = serial.Serial('/dev/ttyUSB0', baudrate=self._baudrate) #Linux
@@ -20,60 +18,33 @@ class StimInterface(object):
         self.gyro = None
         self.skipped_msgs = 0
 
+    #Decodes 3x 3-byte-fixed-point sections
+    def decode_section(self, divisor, byte_offset):
+        return (np.fromstring(self.last_msg[byte_offset+0:byte_offset+3] + b'\x00' +
+                     self.last_msg[byte_offset+3:byte_offset+6] + b'\x00' +
+                     self.last_msg[byte_offset+6:byte_offset+9] + b'\x00',
+                        dtype='>i').astype(np.float32) / (2 ** (divisor+8)))
+
     def read_datagram(self):
         self.last_msg = None
         while self.last_msg != self.datagram_identifier:
             self.last_msg = self.serial.read(1)
-            #print(b'\x93')
-            print(self.last_msg)
-            #time.sleep(1)
         self.last_msg = self.last_msg + self.serial.read(self._read_length)
 
     def decode(self):
-        start = 0
-        print('Decoding')
-        self.gyro = np.fromstring(
-            b'\x00' + self.last_msg[start + 0:start + 3][::-1] +
-            b'\x00' + self.last_msg[start + 3:start + 6][::-1] +
-            b'\x00' + self.last_msg[start + 6:start + 9][::-1],
-            dtype='<i'
-        ).astype(np.float32) / (2 ** 14)
+        self.gyro = self.decode_section(byte_offset=1, divisor=14)
+        self.acc = self.decode_section(byte_offset=11, divisor=19)
+        self.inc = self.decode_section(byte_offset=21, divisor=22)
 
-        start += 10
-        print('Reading ACC')
-        # acc
-        self.acc = np.fromstring(
-            b'\x00' + self.last_msg[start + 0:start + 3][::-1] +
-            b'\x00' + self.last_msg[start + 3:start + 6][::-1] +
-            b'\x00' + self.last_msg[start + 6:start + 9][::-1],
-            dtype='<i'
-        ).astype(np.float32) / (2 ** 19)
-        start += 10
-
-        # inc
-        self.inc = np.fromstring(
-            b'\x00' + self.last_msg[start + 0:start + 3][::-1] +
-            b'\x00' + self.last_msg[start + 3:start + 6][::-1] +
-            b'\x00' + self.last_msg[start + 6:start + 9][::-1],
-            dtype='>i'
-        ).astype(np.float32) / (2 ** 22)
-
-        _gyro = self.gyro
-        _inc = self._inc
-        _acc = self.acc
-
+        self.serial.flushInput()
 
 if __name__ == '__main__':
     i = StimInterface()
     while(True):
-        print('Reading Data')
         i.read_datagram()
-        print('Decoding Data')
         i.decode()
-        print(i.gyro)
-        print(i.acc)
-        print(i.inc)
-        time.sleep(4)
+        print("@", time.clock(), ", gyro:", i.gyro, ", acc:", i.acc, ", inc:", i.inc)
+        time.sleep(1)
 
 
 
